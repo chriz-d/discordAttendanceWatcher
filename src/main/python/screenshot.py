74bfad51118191d8f1e19ef8a85a1533f24ccbc1
chriz <chris.dorn@freenet.de>
2023-07-20 10:32:35 +0200

    refactored code, added args for invocation by bot

--- src/main/python/screenshot.py
+++ src/main/python/screenshot.py
@@ -1,29 +1,65 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 from html2image import Html2Image
 
-# Load css
-styleTag = ""
-with open("src/main/python/style.html", "r") as f:
-    styleTag = f.read() + "\n"
+def main():
+    url = sys.argv[1]
+    savePath = sys.argv[2]
+    style = getCustomCss()
+    content = getHtml(url)
+    createScreenshot(content, style, savePath)
 
-url = "https://simresults.net/230714-3pK"
-# req = requests.get(url)
-req = ""
-with open("src/main/python/results.html", "r") as f:
-    req = f.read()
-soup = BeautifulSoup(req, 'html.parser')
+def getCustomCss():    
+    # Load css
+    styleTag = ""
+    with open("src/main/python/style.html", "r") as f:
+        styleTag = f.read() + "\n"
+    return styleTag
 
-# Clear pesky ads
-results = soup.find_all("div", {"class": ["sideblock-image", "midblock-image"]})
-for result in results:
-    result.decompose()
+def getHtml(url):
+    req = downloadHtml(url)
+    soup = BeautifulSoup(req, 'html.parser')
+    cleanUpTags(soup)
+    content = extractContent(soup)
+    script = extractInlineScript(soup)
+    for key in content:
+        content[key] = content[key] + script
+    return content
 
-# Iterate through desired graphics, append css and screenshot it
-ids = ["race1bestlaps", "race1consistency", "race1sectors", "race1positions-graph"]
-hti = Html2Image(custom_flags=['--window-size=1100,1000', '--hide-scrollbars'])
-for value, id in enumerate(ids):
-    with open("temp.html", "w") as f:
+def downloadHtml(url):
+    # Fetch HTML
+    url = str(sys.argv[1])
+    # req = requests.get(url)
+    req = ""
+    with open("src/main/python/results.html", "r") as f:
+        req = f.read()
+    return req
+
+def extractContent(soup):
+    ids = ["race1bestlaps", "race1consistency", "race1sectors", "race1positions-graph"]
+    content = {}
+    for id in ids:
         tag = soup.find(id=id)
-        hti.screenshot(html_str=styleTag + str(tag.prettify()), save_as=f"out_{value}.png")
-        f.write(styleTag + str(tag.prettify()))
+        content[id] = str(tag.prettify()) 
+    return content
+
+def extractInlineScript(soup):
+    script = soup.find_all("script")[8] # Nice magic number
+    return str(script.prettify())
+
+def cleanUpTags(soup): 
+    # Clear pesky ads
+    results = soup.find_all("div", {"class": ["sideblock-image", "midblock-image"]})
+    for result in results:
+        result.decompose()
+
+def createScreenshot(content, style, path):
+    # Iterate through desired graphics, append css and screenshot it
+    hti = Html2Image(custom_flags=['--window-size=1100,1000', '--hide-scrollbars'], output_path=path)
+    for value, key in enumerate(content):
+        with open("temp.html", "w") as f:
+            hti.screenshot(html_str= style + content[key], save_as=f"out_{value}.png")
+
+if __name__ == "__main__":
+    main()
